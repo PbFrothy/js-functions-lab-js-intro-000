8f9f868e6eecc884a431d5153f540c411dd209ef
Aaron Drake <amdrake01@gmail.com>
2018-12-05 12:37:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // write your code below!
+
+function happyHolidays(){
+  return ("Happy holidays!")
+}
+
+function happyHolidaysTo(name){
+  console.log("Happy holidays, ${name}!");
+}
+
+
+
+function happyHolidaysTo(holiday, name){
+  console.log("Happy ${holiday}, ${name}");
+}
